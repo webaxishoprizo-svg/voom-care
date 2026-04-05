44b583e89112621a4fa490e8c91bd8aace6b0b50
webaxishoprizo-svg <webaxishoprizo-svg@users.noreply.github.com>
2026-04-05 03:25:01 +0000

    Add MostCoveted grid section
    Co-authored-by: webaxishoprizo-svg <233925136+webaxishoprizo-svg@users.noreply.github.com>

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -25,6 +25,7 @@ const Index = () => (
     <ProductGrid />
     <FeaturedProducts />
     <CollectionsSection />
+    <MostCoveted />
     <ExperienceSection />
     <TestimonialsSection />
     <CTASection />
